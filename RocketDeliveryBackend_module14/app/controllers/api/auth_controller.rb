17a3d493a6ac8cec3f67987c9a6e5e7156336890
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-08-25 15:00:44 -0400

    GET et POST fonctionne

--- RocketDeliveryBackend_module14/app/controllers/api/auth_controller.rb
+++ RocketDeliveryBackend_module14/app/controllers/api/auth_controller.rb
@@ -21,13 +21,86 @@ module Api
     # GET /api/account/:id
     # Only covers customer and courier types for now
     def get_account
-      # TODO
+      user_id = params[:id]
+      user_type = params[:type]
+
+      user = User.find_by(id: user_id)
+      if user.nil?
+        render json: { error: "User not found" }, status: :not_found
+        return
+      end
+
+      case user_type
+      when "customer"
+        customer = Customer.find_by(user_id: user.id)
+        if customer.nil?
+          render json: { error: "Customer not found" }, status: :not_found
+          return
+        end
+
+        render json: {
+          email: user.email,
+          customer_email: customer.email,
+          customer_phone: customer.phone
+        }
+      when "courier"
+        courier = Courier.find_by(user_id: user.id)
+        if courier.nil?
+          render json: { error: "Courier not found" }, status: :not_found
+          return
+        end
+
+        render json: {
+          email: user.email,
+          courier_email: courier.email,
+          courier_phone: courier.phone
+        }
+      else
+        render json: { error: "Invalid user type" }, status: :bad_request
+      end
     end
 
     # POST /api/account/:id
     # Currently only covers customer and courier types
     def update_account
-      # TODO
+      user_id = params[:id]
+      user = User.find_by(id: user_id)
+
+      if user.nil?
+        render json: { error: "User not found" }, status: :not_found
+        return
+      end
+
+      data = JSON.parse(request.body.read)
+
+      if data.key?("customer_email") || data.key?("customer_phone")
+        customer = Customer.find_by(user_id: user.id)
+        if customer.nil?
+          render json: { error: "Customer not found" }, status: :not_found
+          return
+        end
+
+        customer.update(
+          email: data["customer_email"] || customer.email,
+          phone: data["customer_phone"] || customer.phone
+        )
+      elsif data.key?("courier_email") || data.key?("courier_phone")
+        courier = Courier.find_by(user_id: user.id)
+        if courier.nil?
+          render json: { error: "Courier not found" }, status: :not_found
+          return
+        end
+
+        courier.update(
+          email: data["courier_email"] || courier.email,
+          phone: data["courier_phone"] || courier.phone
+        )
+      else
+        render json: { error: "Invalid update data" }, status: :bad_request
+        return
+      end
+
+      render json: { success: true }
     end
 
   end
